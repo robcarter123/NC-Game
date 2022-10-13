a5e9c26bb888572edcbca5696e98cff969086cf0
robcarter123 <robcarter@MacBook-Pro.local>
2022-10-13 16:39:34 +0100

    succesfully responds with array of review object and now accept queries

--- models/reviews.model.js
+++ models/reviews.model.js
@@ -1,7 +1,41 @@
-const db = require("../db/connection.js")
+const db = require("../db/connection.js");
+const reviews = require("../db/data/test-data/reviews.js");
+
+const selectReviews = (category) => {
+  const catValues = [
+    "euro_game",
+    "social_deduction",
+    "dexterity",
+    "childrens_games",
+  ];
+
+  const queryValues = [];
+
+  let qStr = `SELECT reviews.*, COUNT(comments.comment_id)::int as comment_count
+  FROM reviews
+  LEFT JOIN comments ON reviews.review_id=comments.review_id`;
+
+  if(category) {
+    if(!catValues.includes(category)){
+    return Promise.reject({ status: 400, message: "Invalid Category"});
+    }
+    qStr += ` WHERE reviews.category=$1`;
+    queryValues.push(category);
+  }
+
+  qStr += ` GROUP BY reviews.review_id
+  ORDER BY reviews.created_at DESC;`;
+
+return db
+    .query(qStr, queryValues).then(( {rows: reviews }) => {
+      console.log(qStr)
+      console.log(queryValues)
+      console.log(reviews)
+      return reviews;
+    })
+  }
 
 const selectReviewById = (review_id) => {
-
     return db
       .query(`SELECT reviews.*, (SELECT COUNT(*)::int FROM comments WHERE review_id=$1)
       AS comment_count FROM reviews WHERE review_id=$1;`,[review_id])
@@ -34,4 +68,4 @@ const updatedReviewsById = (review_id, inc_votes) => {
 
 }
 
-  module.exports = { selectReviewById, updatedReviewsById };
+  module.exports = { selectReviewById, updatedReviewsById, selectReviews };
